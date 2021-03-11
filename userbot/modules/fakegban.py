e5fdec362bccab91ea59648960fe259c34fe0c0a
bryan7379 <79889486+bryan7379@users.noreply.github.com>
2021-03-11 15:09:11 +0700

    Update fakegban.py

--- userbot/modules/fakegban.py
+++ userbot/modules/fakegban.py
@@ -18,9 +18,9 @@ async def gbun(event):
         return
     gbunVar = event.text
     gbunVar = gbunVar[6:]
-    mentions = f"`Warning!! User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n"
+    mentions = f"`Peringatan!! Jamet User 𝙂𝘽𝘼𝙉𝙉𝙀𝘿 By` {DEFAULTUSER}\n"
     no_reason = "No Reason Given "
-    await event.edit("**Summoning out the mighty gban hammer ☠️**")
+    await event.edit("**Gausah jadi jamet tolol ☠️**")
     asyncio.sleep(3.5)
     chat = await event.get_input_chat()
     async for x in bot.iter_participants(chat, filter=ChannelParticipantsAdmins):
